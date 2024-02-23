dd1e131d1c5f0761e7796e928051334bcd0df9c7
Diego Moura <programmercodeoficial@gmail.com>
2024-02-23 19:30:04 -0300

    inicia projeto

---
+++ src/main/java/com/br/diegomoura/springbootwebapi/SpringbootWebApiApplication.java
@@ -0,0 +1,13 @@
+package com.br.diegomoura.springbootwebapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootWebApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootWebApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/br/diegomoura/springbootwebapi/SpringbootWebApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.diegomoura.springbootwebapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootWebApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
